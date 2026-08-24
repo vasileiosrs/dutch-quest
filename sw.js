ef70e55589cedb8a97b997442537bd78970e65e5
vasileiosrs <vasileiosrs@gmail.com>
2026-08-24 11:22:39 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dutch-quest-v3-pwa-3';
+const CACHE='dutch-quest-v3-pwa-5';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(
